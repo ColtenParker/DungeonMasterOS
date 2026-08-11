a75317a463de3997bc4a9ec47c4d4d69d06d72ab
Colten Parker <coltenparker00@gmail.com>
2026-08-11 10:18:44 -0400

    fixed env

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,4 +1,6 @@
-import "dotenv/config";
+import { config } from "dotenv";
+
+config({ path: new URL("../../../.env", import.meta.url) });
 
 import { createApp } from "./app.js";
 import { databaseHealth, prisma } from "./database.js";
